ef981d95628bbe4a9359714e0f58ae6260adc539
Chirangmon <dlzjsgml@gmail.com>
2021-09-11 13:23:42 +0900

    한글 수정

--- src/gitTest/Test.java
+++ src/gitTest/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, World");
-		System.out.println("�־ȵǴ�.....");
+		System.out.println("한글이 써지나요?");
 		
 	}
 
